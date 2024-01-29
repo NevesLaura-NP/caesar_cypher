e8ee3e40b2e497118a96db20819b6403707b0510
Le_Nev <109705179+NevesLaura-NP@users.noreply.github.com>
2024-01-28 23:09:31 -0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -8,19 +8,18 @@ def encrypt(message, k):
     
     # Loops through letters inside the list
     for i in split_message:
-        # Creates a variable that has the message translated into ASCII 
+        # checks for upper case letter
         if i.isupper():          
-            #creates variable that turns letters into ASCII then checks for the uppercase limit
-            encrypted_ascii = (ord(i) + k) % 94 
-            if encrypted_ascii < 65:
-                encrypted_ascii += 65
+            # Creates variable that turns letters into ASCII
+            # Subtracts minimum ASCII for letters
+            # Adds key value
+            # Calculates the remainder of the amount of letters in the alphabet
+            # Reinserts minimum to count ASCII letters
+            encrypted_ascii = (ord(i) - 65 + k) % 26 + 65
             encrypted_message = chr(encrypted_ascii)
+        # checks for lower case letter
         elif i.islower():
-            #creates variable that turns letters into ASCII then checks for the lowercase limit
-            encrypted_ascii = (ord(i) + k) % 123 
-            # Checks if value of ASCII is under 97 and if it is, adds 97
-            if encrypted_ascii < 97:
-                encrypted_ascii += 97
+            encrypted_ascii = (ord(i) - 97 + k) % 26 + 97 
             # Translates message into letters according to ASCII
             encrypted_message = chr(encrypted_ascii)
         else: 
@@ -32,31 +31,21 @@ def encrypt(message, k):
 
 # Function that decrypts message variable
 def decrypt(message, k):
-    # Creates a new empty variable to store new decrypted message
     new_message = ""
     
-    # Splits string into separate letters
     split_message = list(message)
+    
     for i in split_message:
-        # Checks if the letter is upper case
         if i.isupper():
-            #creates variable that turns letters into ASCII then checks for the uppercase limit
-            decrypted_ascii = (ord(i) - k ) % 92 
-            if decrypted_ascii < 65:
-                # If ASCII is lower than 65, it creates a variable that makes the counting loop back to counting only uppercase letters
-                loop = 65 - decrypted_ascii
-                decrypted_ascii = 123 - loop 
-            # Translates message into letters according to ASCII
+            # Creates variable that turns letters into ASCII
+            # Subtracts minimum ASCII for letters
+            # Calculates the remainder of the amount of letters in the alphabet
+            # Subtracts Key value
+            # Reinserts minimum to count ASCII letters
+            decrypted_ascii = (ord(i) - 65 - k) % 26 + 65
             decrypted_message = chr(decrypted_ascii)
-        # Checks if the letter is lower case
         elif i.islower():
-           #creates variable that turns letters into ASCII then checks for the lowercase limit
-            decrypted_ascii = (ord(i) - k) % 123
-            # Checks if ASCII value is under 97
-            if decrypted_ascii < 97:
-                # If ASCII is lower than 97, it creates a variable that makes the counting loop back to counting only lowercase letters
-                loop = 97 - decrypted_ascii
-                decrypted_ascii = 123 - loop 
+            decrypted_ascii = (ord(i) - 97 - k) % 26 + 97
             # Translates message into letters according to ASCII
             decrypted_message = chr(decrypted_ascii)
         else:
@@ -66,11 +55,11 @@ def decrypt(message, k):
     
     return new_message
 
-message = "Holidays are over"
-k = 10
+message = "Hello Brand New World"
+k = 1000000
 
 encrypted = encrypt(message, k)
 decrypted = decrypt(encrypted, k)
 
-print("Your encrypted code is:", encrypted)
-print("Your decrypted code is:", decrypted)
+print("Your encrypted message is:", encrypted)
+print("Your decrypted message is:", decrypted)
